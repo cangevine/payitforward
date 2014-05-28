e9e5405a9e7e2175a52253117198f64033052cd5
Colin Angevine <info@linguazone.com>
2014-05-28 09:37:51 -0400

    Added first draft of Alec's changes.

--- js/index.js
+++ js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -37,6 +19,7 @@ var app = {
     // function, we must explicity call `app.receivedEvent(...);`
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+		console.log("Anyone home??? Device is now ready...!");
     },
 
     // Update DOM on a Received Event
@@ -52,22 +35,24 @@ var app = {
     },
 
     scan: function() {
-        console.log('scanning');
+        console.log('scanning...!');
         
         var scanner = cordova.require("cordova/plugin/BarcodeScanner");
-
+		
+		console.log(scanner);
         scanner.scan( function (result) { 
-
+			
+            console.log("Scanner result: \n" +
+                 "text: " + result.text + "\n" +
+                 "format: " + result.format + "\n" +
+                 "cancelled: " + result.cancelled + "\n");
+			
             alert("We got a barcode\n" + 
             "Result: " + result.text + "\n" + 
             "Format: " + result.format + "\n" + 
             "Cancelled: " + result.cancelled);  
-
-           console.log("Scanner result: \n" +
-                "text: " + result.text + "\n" +
-                "format: " + result.format + "\n" +
-                "cancelled: " + result.cancelled + "\n");
-            document.getElementById("info").innerHTML = result.text;
+            //document.getElementById("scanResult").innerHTML = result.text;
+			sendQRToDB(result.text);
             console.log(result);
             /*
             if (args.format == "QR_CODE") {
@@ -81,9 +66,10 @@ var app = {
     },
 
     encode: function() {
+		console.log("Trying to encode now!");
         var scanner = cordova.require("cordova/plugin/BarcodeScanner");
 
-        scanner.encode(scanner.Encode.TEXT_TYPE, "http://www.nhl.com", function(success) {
+        scanner.encode(scanner.Encode.TEXT_TYPE, "http://www.friendscentral.org", function(success) {
             alert("encode success: " + success);
           }, function(fail) {
             alert("encoding failed: " + fail);
